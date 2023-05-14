42149ae5174df7e85c89d0fa8089be425aa46da4
xc1614 <xc1614@nyu.edu>
2023-05-14 14:47:16 -0400

    Crowdfunding Database

---
+++ crowdfunding_db.sql
@@ -0,0 +1,64 @@
+--create table category
+
+
+
+CREATE TABLE category (
+    category_id VARCHAR(10)   NOT NULL,
+    category VARCHAR(50)   NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
+SELECT * FROM category;
+
+--create table subcategory
+
+
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(10)   NOT NULL,
+    subgetegory VARCHAR(50)   NOT NULL,
+    PRIMARY KEY (subcategory_id)
+);
+SELECT * FROM subcategory;
+
+--create table contacts
+
+CREATE TABLE contacts (
+    contact_id INT   NOT NULL,
+    first_name VARCHAR(50)   NOT NULL,
+    last_name VARCHAR(50)   NOT NULL,
+    email VARCHAR(100)   NOT NULL,
+    PRIMARY KEY (contact_id)
+);
+
+SELECT * FROM contacts;
+
+--create table campaign
+
+CREATE TABLE campaign (
+    cf_id INT NOT NULL,
+    contact_id INT NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    goal NUMERIC NOT NULL,
+    pledged NUMERIC NOT NULL,
+    outcome VARCHAR(20) NOT NULL,
+    backers_count INT NOT NULL,
+    country VARCHAR(10) NOT NULL,
+    currency VARCHAR(10) NOT NULL,
+    launched_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    category_id VARCHAR(10) NOT NULL,
+    subcategory_id VARCHAR(50) NOT NULL,
+    FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
+SELECT * FROM campaign;
+
+
+DROP TABLE category;
+DROP TABLE subcategory;
+DROP TABLE contacts;
+DROP TABLE campaign;
+
